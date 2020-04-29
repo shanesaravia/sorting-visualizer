940253e6e1431872867fdf027e0e83b3cef22e46
Shane Saravia <shanesaravia@live.ca>
2020-04-29 03:06:45 -0400

    quicksort fixed

--- src/helpers/algorithms/quickSort.js
+++ src/helpers/algorithms/quickSort.js
@@ -3,24 +3,17 @@ const partition = (arr, left, right, animations) => {
   let pivot = arr[pivotIdx]; //middle element
   let i = left; //left pointer
   let j = right; //right pointer
-  // console.log('pivot: ', pivot);
   
   while (i <= j) {
-    // animations.push([i, j, false, pivotIdx, false])
-    // console.log('while outer i: ', i)
-    // console.log('while outer j: ', j)
     while (arr[i] < pivot) {
-      // console.log('while 1 i: ', j)
       animations.push([i, j, false, pivotIdx, false, pivot])
       i++;
     }
     while (arr[j] > pivot) {
-      // console.log('while 2 j: ', j)
       animations.push([i, j, false, pivotIdx, false, pivot])
       j--;
     }
     if (i <= j) {
-      // console.log('while if ij: ', j)
       animations.push([i, j, true, pivotIdx, false, pivot])
       // Swap two elements
       let temp = arr[i];
